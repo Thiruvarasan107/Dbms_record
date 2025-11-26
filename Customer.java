c22540996c1b38c17410f23eeafe6e24ee0cd377
Thiruvarasan107 <thiruvarasan.sg.2024.csecs@rajalakshmi.edu.in>
2025-11-26 21:52:37 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,32 @@
+model/Customer.java
+
+package model;
+
+public class Customer {
+    private int customerId;
+    private String name;
+    private String phone;
+    private String address;
+
+    public Customer() {}
+
+    public Customer(int customerId, String name, String phone, String address) {
+        this.customerId = customerId;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    // getters & setters
+    public int getCustomerId() { return customerId; }
+    public void setCustomerId(int customerId) { this.customerId = customerId; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+}
